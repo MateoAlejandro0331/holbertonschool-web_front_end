88475f9af42d3c4afd6ceea9f61b99fc88a768f8
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-01-19 19:56:50 -0500

    adding all files until to task 3

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+function welcome(firstName, lastName) {
+    
+    function displayFullName () {
+        alert('Welcome ' + firstName + ' ' + lastName)
+    }
+    displayFullName()
+}
